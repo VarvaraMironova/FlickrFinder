bc59e12cb570b8864eaf9a217db6abbaa06811e2
Varvara Mironova <varvaramironova@MacBook-Air-Varvara.local>
2015-07-21 10:20:42 +0300

    Initial commit

---
+++ FlickFinder/RootView.swift
@@ -0,0 +1,30 @@
+//
+//  RootView.swift
+//  FlickFinder
+//
+//  Created by Varvara Mironova on 7/20/15.
+//  Copyright (c) 2015 VarvaraMironova. All rights reserved.
+//
+
+import UIKit
+
+class RootView: UIView {
+    @IBOutlet var headerLabel           : UILabel!
+    @IBOutlet var flickrImageView       : UIImageView!
+    @IBOutlet var searchTextField       : UITextField!
+    @IBOutlet var longitudeTextField    : UITextField!
+    @IBOutlet var latitudeTextField     : UILabel!
+    @IBOutlet var searchTextButton      : UIButton!
+    @IBOutlet var searchCoordinateButton: UIButton!
+    @IBOutlet var latitudeLabel         : UILabel!
+    @IBOutlet var longitudeLabel        : UILabel!
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FlickFinder/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  FlickFinder
+//
+//  Created by Varvara Mironova on 7/20/15.
+//  Copyright (c) 2015 VarvaraMironova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var rootView: RootView!
+    
+    @IBAction func onSearchTextButton(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onSearchCoordinateButton(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
